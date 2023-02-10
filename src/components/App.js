1cc112b08d1d360f8183f07a2fb9095a3cec346b
koma4028 <koma4028@gmail.com>
2023-02-10 23:38:57 +0900

    Commit Until #2.5 Social Login

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AppRouter from "components/Router";
 import { myFirebaseApp, fbAuth } from "firebaseInstance";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = React.useState((fbAuth.currentUser === null ? false : true));
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+  // Use Hook to add 'onAuthStateChanged' Event Lister to 'useEffect'(= 'Mounting Components' Event Lister)
+  useEffect(() => {
+    fbAuth.onAuthStateChanged((user) => {
+      user ? setIsLoggedIn(true) : setIsLoggedIn(false);
+      setInit(true);
+    });
+  }, [])
   return (
     <>
-      <AppRouter
-        isLoggedIn={isLoggedIn}
-      />
-      <footer>
+      {/* Show 'AppRouter' only after initializing is finished */}
+      {init ?
+        <AppRouter
+          isLoggedIn={isLoggedIn}
+        />
+        : "Initializing..."
+      }
+      < footer >
         &copy; {new Date().getFullYear()} Nwitter
       </footer>
+
     </>
   );
 }
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,24 +1,46 @@
-import React from "react";
+import { fbAuth } from "firebaseInstance";
+import React, { useState } from "react";
 
 const Auth = () => {
     const [email, setEmail] = React.useState("");
     const [password, setPassword] = React.useState("");
+    const [newAccount, setNewAccount] = React.useState(true);
+    const [error, setError] = useState("");
+
     const onChange = (event) => {
-        const {target: {name, value}} = event;
+        const { target: { name, value } } = event;
         //const {name, value} = {event.target.name, event.target.value};
         if (name === "email") setEmail(value);
         else if (name === "password") setPassword(value);
     };
-    const onSubmit = (event) => {
+
+    const onSubmit = async (event) => {
         event.preventDefault();
+        try {
+            let userData;
+            if (newAccount) {
+                // Create Account
+                userData = await fbAuth.createUserWithEmailAndPassword(email, password);
+            } else {
+                // Login
+                userData = await fbAuth.signInWithEmailAndPassword(email, password);
+            }
+            console.log(userData);
+        } catch (error) {
+            setError(error.message);
+        }
     };
+
+    const toggleNewAccount = () => {
+        setNewAccount((prev) => !prev);
+    }
 
     return (
         <div>
             <form onSubmit={onSubmit}>
                 <input
                     name="email"
-                    type="text"
+                    type="email"
                     placeholder="Email"
                     required
                     value={email}
@@ -32,8 +54,10 @@ const Auth = () => {
                     value={password}
                     onChange={onChange}
                 />
-                <input type="submit" value="Login" />
+                <input type="submit" value={newAccount ? "Create Account" : "Sign In"} />
             </form>
+            {error}
+            <span onClick={toggleNewAccount}>{newAccount ? "Sign In" : "Create Account"}</span>
             <div>
                 <button>Continue with Google</button>
                 <button>Continue with GitHub</button>
